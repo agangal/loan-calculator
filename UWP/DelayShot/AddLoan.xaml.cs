55a2d98aec8319432f42eca5c438aa70cef30a94
Ashish Gangal <ashish.gangal1990@gmail.com>
2018-10-22 15:36:18 -0700

    worked on ui some more

--- UWP/DelayShot/AddLoan.xaml.cs
+++ UWP/DelayShot/AddLoan.xaml.cs
@@ -26,5 +26,10 @@ namespace DelayShot
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
